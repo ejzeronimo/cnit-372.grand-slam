544e75caff53050f4b5de86ce8698f9076556308
Ethan Zeronik <ejzeronik@gmail.com>
2022-11-01 15:37:11 -0400

    basic custom page made

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,39 +2,13 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/index.module.css'
 
-const oracledb = require('oracledb');
 
-
-async function getCurrentDate(): Promise<string> {
-  let connection = await oracledb.getConnection({ user: "demonode", password: "XXXX", connectionString: "localhost/xepdb1" });
-
-  var result = await connection.execute(
-    `select sysdate from dual;`,
-    [],
-    { resultSet: true, outFormat: oracledb.OUT_FORMAT_OBJECT });
-
-  // const rs = result.resultSet;
-  // let row;
-
-  // while ((row = await rs.getRow())) {
-  //   if (row.DONE)
-  //     console.log(row.DESCRIPTION, "is done");
-  //   else
-  //     console.log(row.DESCRIPTION, "is NOT done");
-  // }
-  return "asd";
-}
-
-
-let foo = await getCurrentDate();
-
-
-export default async function render() {
+export default function render() {
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>${foo}</title>
+        <title>Hello World</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
